210cb4e62c78a32753e3b59391fc182d8455f125
NeyNey <drfrankenstein123@gmail.com>
2020-10-14 11:39:50 -0500

    added inquirer questions

--- server.js
+++ server.js
@@ -18,3 +18,39 @@ connection.connect(function(err){
     console.log("connected as id" + connection.threadId);
     start();
 });
+
+
+function start() {
+    inquirer
+        .prompt({
+            name: "task",
+            type: "list",
+            message: "the choice is yours, what will you do?",
+            choices: [
+                "Add bussiness role",
+                "Add department",
+                "view bussiness roles",
+                "view departments",
+                "view employees",
+                "update an employee",
+                "bye bye"
+            ]
+        })
+        .then(function (answer){
+            if (answer.task === "Add bussiness role"){
+                addBussinessRole();
+            }else if (answer.task === "Add department"){
+                addDepartment();
+            }else if (answer.task === "view bussiness roles"){
+                viewBussinessRoles();
+            }else if (answer.task === "view departments"){
+                viewDepartments();
+            }else if (answer.task === "view employees"){
+                viewEmployees();
+            }else if (answer.task === "update an employee"){
+                console.log("employee updated")
+            }else{
+                connection.end();
+            }
+        })
+}
