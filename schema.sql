1f4077ada41445484d9ce79ccc54480b4461e5f3
neyneyalldayday <drfrankenstein123@gmail.com>
2023-07-21 18:13:03 -0500

    havent touched this in a while, knock the dust off

--- schema.sql
+++ schema.sql
@@ -1,20 +1,21 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE department (
-	id INT PRIMARY KEY NOT NULL,
+	id INT PRIMARY KEY AUTO_INCREMENT ,
     department_name VARCHAR (30) NOT NULL
     ); 
     
     CREATE TABLE business_role (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT ,
     title VARCHAR (30) NOT NULL,
     salary DECIMAL (10,4) NOT NULL,
     department_id INT NOT NULL
     );
     
     CREATE TABLE employee (
-		id INT PRIMARY KEY NOT NULL,
+		id INT PRIMARY KEY AUTO_INCREMENT ,
         first_name VARCHAR (30) NOT NULL,
         last_name VARCHAR (30) NOT NULL,
         role_id INT NOT NULL,
--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-USE employeetracker_db;
+USE employeeTracker_db;
 
 INSERT INTO department (department_name)
 VALUES ("sales"),
@@ -6,17 +6,18 @@ VALUES ("sales"),
 ("finance"),
 ("support");
 
-INSERT INTO business_role (title, salary)
-VALUES ("legal team lead","250000"),
-("regional manager", "100000"),
-("head Accountaint", "90000"),
-("salesman", "40000"),
-("customer service rep", "35000");
+INSERT INTO business_role (title, salary, department_id)
+VALUES ("legal team lead", "250000", 1),
+("regional manager", "100000", 2),
+("head Accountaint", "90000", 4),
+("salesman", "40000", 2),
+("customer service rep", "35000", 1);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("sarah", "lourd"),
-("val", "kilmer"),
-("bobby", "bouchier"),
-("taz" , "the devil"),
-("bill" , "paxton")
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("sarah", "lourd", 4, NULL),
+("val", "kilmer", 4, NULL),
+("bobby", "bouchier", 4, NULL),
+("taz", "the devil", 4, NULL),
+("bill", "paxton", 4, NULL);
+
 
